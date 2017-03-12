55ee4c2bd2c2b7043027acb1ff0da27eb7a351b5
Gaurav Singh <gauravsingh@circuitvalley.com>
2017-03-12 17:32:51 +0100

    transmitter_source

---
+++ Sensor_transmitter/ds18b20.h
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/** Descriptive File Name
+
+  @Company
+ CircuitValley
+
+  @File Name
+    filename.h
+
+  @Summary
+    Brief description of the file.
+
+  @Description
+    Describe the purpose of this file.
+ */
+
+#ifndef _DS1820_H
+#define _DS1820_H
+
+#include <msp430.h>
+#include <stdint.h>
+/* check configuration of driver */
+
+#define PIN_ONE_WIRE 		BIT4
+#define PORT_ONE_WIRE		P2OUT
+#define PORT_ONE_WIRE_IN	P2IN
+#define PORT_ONE_DIR		P2DIR
+#define setOneWire(b) 		PORT_ONE_WIRE=(b)?(PORT_ONE_WIRE|PIN_ONE_WIRE):PORT_ONE_WIRE&(~PIN_ONE_WIRE)
+#define setOneWireDir(b) 	PORT_ONE_DIR=(b)?(PORT_ONE_DIR|PIN_ONE_WIRE):PORT_ONE_DIR& (~PIN_ONE_WIRE)
+#define getOneWire  		(((PORT_ONE_WIRE_IN)&PIN_ONE_WIRE)&&0x1)
+#define setOneWireREN		P2REN|=PIN_ONE_WIRE
+#define TEMP_RES              0x100 /* temperature resolution => 1/256°C = 0.0039°C */
+
+
+#define ENABLE_SENSOR_POWER() 	P2DIR|=BIT3; P2OUT|=BIT3;
+#define DISABLE_SENSOR_POWER()	P2DIR&=~BIT3; P2OUT&=~BIT3; setOneWireDir(LOW);
+
+#define LOW 0
+#define HIGH 1
+
+#define fake_OC_setOneWire(b) setOneWireDir(!b);
+/* -------------------------------------------------------------------------- */
+/*                         DS1820 Timing Parameters                           */
+/* ------------------------------------------------------------------------- */
+#define system_clock 8 //in MHz
+#define us_to_ncyles(a)  a*system_clock
+
+#define DS1820_RST_PULSE       us_to_ncyles(480) //480us   /* master reset pulse time in [us] */
+#define DS1820_MSTR_BITSTART   us_to_ncyles(2)  //2us     /* delay time for bit start by master */
+#define DS1820_PRESENCE_WAIT   us_to_ncyles(40)     //40    /* delay after master reset pulse in [us] */
+#define DS1820_PRESENCE_FIN    us_to_ncyles(480) //480   /* dealy after reading of presence pulse [us] */
+#define DS1820_BITREAD_DLY     us_to_ncyles(5)     //5     /* bit read delay */
+#define DS1820_BITWRITE_DLY    us_to_ncyles(80) //100   /* bit write delay */
+
+#define DS1820_DELAY_750MS 	   us_to_ncyles(750000)
+
+/* -------------------------------------------------------------------------- */
+/*                            DS1820 Registers                                */
+/* -------------------------------------------------------------------------- */
+
+#define DS1820_REG_TEMPLSB    0
+#define DS1820_REG_TEMPMSB    1
+#define DS1820_REG_CNTREMAIN  6
+#define DS1820_REG_CNTPERSEC  7
+#define DS1820_SCRPADMEM_LEN  9     /* length of scratchpad memory */
+
+#define DS1820_ADDR_LEN       8
+
+
+/* -------------------------------------------------------------------------- */
+/*                            DS1820 Commands                                 */
+/* -------------------------------------------------------------------------- */
+
+#define DS1820_CMD_SEARCHROM     0xF0
+#define DS1820_CMD_READROM       0x33
+#define DS1820_CMD_MATCHROM      0x55
+#define DS1820_CMD_SKIPROM       0xCC
+#define DS1820_CMD_ALARMSEARCH   0xEC
+#define DS1820_CMD_CONVERTTEMP   0x44
+#define DS1820_CMD_WRITESCRPAD   0x4E
+#define DS1820_CMD_READSCRPAD    0xBE
+#define DS1820_CMD_COPYSCRPAD    0x48
+#define DS1820_CMD_RECALLEE      0xB8
+
+
+#define DS1820_FAMILY_CODE_DS18B20      0x28
+#define DS1820_FAMILY_CODE_DS18S20      0x10
+
+typedef struct DS18B20
+{
+    uint16_t temp; //Q12.4 format
+    uint8_t ROM[8];
+}DS_sensor_t;
+extern DS_sensor_t tsensor;
+
+void findDS18B20(DS_sensor_t *sensor);
+
+void readTemp(DS_sensor_t *sensor);
+void initOneWire();
+
+/* -------------------------------------------------------------------------- */
+/*                            static variables                                */
+/* -------------------------------------------------------------------------- */
+
+
+#endif /* _DS1820_H */
+
---
+++ Sensor_transmitter/main.c
@@ -0,0 +1,121 @@
+#include <msp430.h> 
+#include <stdint.h>
+#include "ds18b20.h"
+#include "eusci_b_spi.h"
+#include "NRF24L01P.h"
+/*
+ * File:   main.c
+ * Author:  Gaurav
+ * website: www.circuitvalley.com
+ * Created on March 14, 2017, 9:42 PM
+ *	This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *	Email: gauravsingh@circuitvalley.com
+************************************************************************/
+
+
+
+// measured Power consuption
+// ~1.7 mA for 600ms during Temp Sampling,
+// ~1.0 mA for 140ms during NRF SPI and Setup
+// ~0.8 mA for 40ms
+// ~2.0 mA average for 20ms time around actual RF transmission though peak goes around max 9ma for short period of time ~8ms,
+static void initClock()
+{
+	PM5CTL0=0;
+	CSCTL0 =0xA500;
+	// FRCTL0 = 0xA500 | NWAITS0; //keine warte weil 8Mhz clk
+	CSCTL1 = DCOFSEL_3;  // on reset set to 8MHz
+	CSCTL2 = SELA__VLOCLK |  SELS__DCOCLK | SELM__DCOCLK;
+	CSCTL3 &= ~(DIVA0 | DIVA1 | DIVA2 | DIVS0 | DIVS1 | DIVS2 | DIVM0| DIVM1 | DIVM2  ); // MCLK and SMCLK to be 8Mhz
+	//CSCTL3 |= DIVA1|DIVA0;  // ACLK = CLK/8 ==  1Mhz
+	// CSCTL4 = HFXTDRIVE0|HFXTDRIVE1 | HFFREQ1 |LFXTOFF  ;  //drive HFXT to be max power ( kein crytal bei stromsucher V1.0 deshab nicht benötigt)
+	// _delay_cycles(5000);
+	// CSCTL0 =0x0000;
+	SFRIFG1 &=~( OFIFG);
+}
+
+
+void initSamplingTIMERB1()
+{
+	TB1CTL = 0;
+	TB1CCR0 =60000; //VLO is minimum 5 khz , VLO /5 is minimum 1khz , at 1khz 60000 for 60 seconds
+
+	TB1EX0 = TBIDEX_4; // div by 5
+	TB1CTL = TASSEL__ACLK | ID_0 | MC_1  |TBIE|TBCLR;
+	TB1R=58000;
+}
+
+
+
+#pragma vector = TIMER1_B1_VECTOR		//timer a0 over flor make it trig ever 333ms update display here
+__interrupt void samplingTIMERB1_ISR(void)
+{
+	if(TB1CTL&TBIFG)
+	{	uint16_t integer_temp;
+		uint8_t sign,fraction_temp;
+		ENABLE_SENSOR_POWER();
+		readTemp(&tsensor);
+		DISABLE_SENSOR_POWER();
+		integer_temp=tsensor.temp>>4;
+		fraction_temp=tsensor.temp&0xf;
+		sign='+';
+		if(integer_temp&0x800)
+		{
+			integer_temp=(~integer_temp)&0xFF ; //number is "extended" 2's complement
+			sign='-';
+			fraction_temp= (~fraction_temp)&0xf;
+
+
+		}
+		fraction_temp=(fraction_temp*625)/1000;
+		TxBuf[1]=(integer_temp/100)+0x30;
+		if(TxBuf[1]=='0')
+				{
+					TxBuf[0]=' ';
+					TxBuf[1]=sign;
+				}else
+				{
+					TxBuf[0]=sign;
+				}
+				TxBuf[2]= ((integer_temp/10)%10) +0x30 ;
+				if(TxBuf[2]=='0' && TxBuf[1]==sign)
+				{
+						TxBuf[1]=' ';
+						TxBuf[2]=sign;
+				}
+
+				TxBuf[3]= (integer_temp%10)+0x30;
+
+		TxBuf[4]='.';
+		TxBuf[5]=fraction_temp+0x30;
+		TxBuf[6]='\r';
+		TxBuf[7]='\n';
+
+		nRF24L01_TxPacket(TxBuf);	// send the buffer packet
+		SPI_RW_Reg(WRITE_REG+STATUS,0XFF);
+	}
+	TB1CTL &= ~TBIFG;
+}
+
+
+int main(void) {
+	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer for a while
+	initClock();
+	initSamplingTIMERB1();
+	initOneWire();
+	findDS18B20(&tsensor);
+	initNRF24L01pIO();
+	initNRF24L01p() ;
+	__enable_interrupt();
+	_BIS_SR(LPM3_bits + GIE);
+	__no_operation();
+}
